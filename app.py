ba0e49540b9d75a9dbd3609bb4b60519918a05d5
Numanssss <numansaruhanns@gmail.com>
2026-08-06 00:29:17 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -36,10 +36,15 @@ def get_transparent_logo_base64():
         img = Image.open(logo_path).convert("RGBA")
         datas = img.getdata()
         new_data = []
-        # Beyaz ve yakın tonlarını şeffafa çevir
+        # Make white background transparent and white text dark slate for light mode
         for item in datas:
-            if item[0] > 240 and item[1] > 240 and item[2] > 240:
+            if len(item) == 4 and item[3] < 50:
                 new_data.append((255, 255, 255, 0))
+            elif item[0] > 220 and item[1] > 220 and item[2] > 220:
+                if len(item) == 4 and item[3] >= 50:
+                    new_data.append((15, 23, 42, 255))
+                else:
+                    new_data.append((255, 255, 255, 0))
             else:
                 new_data.append(item)
         img.putdata(new_data)
